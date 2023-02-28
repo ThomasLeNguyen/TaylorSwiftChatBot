3604585111fa7bc31e38e1c3d8d09af5a32f1aef
Thomas <thomaslenguyenasian@gmail.com>
2023-02-27 17:08:03 -0800

    Update chat.py
    Updated

--- things/chat.py
+++ things/chat.py
@@ -47,8 +47,7 @@ class chat(actor):
              #       break
 
         if msg == None:
-            return  random.choice( CORPUS['misc corpus'] )
+            return  random.choice( CORPUS[ 'misc corpus' ] ) 
         else:
-            return [ msg ]
-
+            return msg
 
